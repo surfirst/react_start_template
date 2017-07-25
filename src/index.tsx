7e3d520e88c7205aefca8cad931203400e65ff43
Leo Yee <surfirst@yeah.net>
2017-07-25 18:40:28 +0800

    template created

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+
+import * as React from "react";
+import { render } from "react-dom";
+import {App} from "./components/App";
+
+console.log("Test5!");
+
+render(
+     <App />,
+     document.getElementById("root")    
+);
